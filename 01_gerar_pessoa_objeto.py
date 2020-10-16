994feeb78ffe47674add28dc3bbfdff58727abd6
Andrei Zamoiski <andrei@zmk.com.br>
2020-10-16 19:24:34 -0300

    upload

--- 01_gerar_pessoa_objeto.py
+++ 01_gerar_pessoa_objeto.py
@@ -1,6 +1,7 @@
 import random
 import datetime
 import requests
+import buscacep
 
 class Pessoa:
 
@@ -12,6 +13,8 @@ class Pessoa:
         self.data_nascimento = self.gerarDataNascimento()
         self.idade = abs(int(self.data_nascimento.split("-")[0]) - datetime.datetime.now().year)
         self.telefone = self.gerarNumeroTelefone()
+        self.geraEndereço()
+        self.tipoSanguineo = ['A-', 'A+', 'B-', 'B+', 'AB-', 'AB+', 'O-', 'O+'][random.randrange(8)]
         
     def criarCPF(self):
         def calcula_digito(digs):
@@ -28,7 +31,7 @@ class Pessoa:
         return ''.join(map(str, n))
 
     def gerarNome(self, sexo):
-        file = '01_nomes_masculinos.csv' if sexo == "M" else '01_nomes_femininos.csv'
+        file = 'nomes_masculinos.csv' if sexo == "M" else 'nomes_femininos.csv'
         rand = random.randint(0,1000)
         with open(file, 'r') as lista:
             for i, line in enumerate(lista):
@@ -38,7 +41,7 @@ class Pessoa:
 
     def gerarSobrenome(self):
         rand = random.randrange(90) if random.random() > 0.5 else random.randrange(1800)
-        with open('01_listadesobrenomesbrasileiros.txt', 'r', encoding = 'utf=8') as nomes:
+        with open('listadesobrenomesbrasileiros.txt', 'r', encoding = 'utf=8') as nomes:
             for i, line in enumerate(nomes):
                 if i == rand:
                     return line.split(',')[0]
@@ -46,13 +49,27 @@ class Pessoa:
     def gerarDataNascimento(self):
         return f"{random.randrange(1970, 2002)}-{str(random.randrange(1,12)).zfill(2)}-{str(random.randrange(1,28)).zfill(2)}"
 
-    def gerarNumeroTelefone(self, ddd=41, fixo=False):
-        tipo = ' ' if fixo else ' 9'
-        numero = f'{ddd}{tipo}'
+    def gerarNumeroTelefone(self, fixo=False):
+        tipo = '' if fixo else '9'
+        numero = f'{tipo}'
         numero += str(random.randint(1,9))
         for n in range(7):
             numero += str(random.randint(0,9))
         return numero
+
+    def geraEndereço(self):
+        rand = random.randint(0,732763)
+        with open("lista_ceps.txt", "r", encoding="utf-8") as ceps:
+            for linha, cep in enumerate(ceps):
+                if rand == linha:
+                    cep_aleatorio = cep
+        
+        endereço= buscacep.busca_cep_correios_as_dict(cep_aleatorio[:8])
+        self.rua = endereço['logradouro']
+        self.cidade, self.estado = endereço['localidade'].split("/")
+        self.bairro = endereço['bairro']
+        self.cep = endereço['cep']
+        self.numero = random.randint(1,700)
 
 if __name__ == "__main__":
     
@@ -66,8 +83,14 @@ if __name__ == "__main__":
             "cpf" : f"{novaPessoa.cpf}",
             "idade" : f"{novaPessoa.idade}",
             "nascimento" : f"{novaPessoa.data_nascimento}",
-            "telefone" : f"{novaPessoa.telefone}"
+            "telefone" : f"{novaPessoa.telefone}",
+            "rua" : f"{novaPessoa.rua}",
+            "número": f"{novaPessoa.numero}",
+            "cidade" : f"{novaPessoa.cidade}",
+            "bairro" : f"{novaPessoa.bairro}",
+            "cep" : f"{novaPessoa.cep}",
+            "tipo sanguíneo" : f"{novaPessoa.tipoSanguineo}"
         }
         
-        r = requests.post(url, json=dados, headers=headers)
-        print(r)
+        #r = requests.post(url, json=dados, headers=headers)
+        print(dados)
